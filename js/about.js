0599900a23f4d540995164a2b490d80aee6bf4c2
Hernan Rodriguez <hernan.rod.98@gmail.com>
2020-11-09 19:55:19 -0300

    Uso de Vue
    Se implementa el framework de JavaScript para añadir dinamicamente los empleados

---
+++ js/about.js
@@ -0,0 +1,23 @@
+app = new Vue({
+    el:"#app",
+    data:{
+        staff:[
+            {name: "John Doe",
+            img: "../img/avatar.png",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. At corrupti, ullam quo ducimus doloribus expedita labore dolorum molestiae tenetur, hic voluptatem, voluptas odio aliquid vero totam perferendis quam animi consectetur?"              
+            },
+            {name: "Sally Housecoat",
+            img: "../img/avatar_2.png",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. At corrupti, ullam quo ducimus doloribus expedita labore dolorum molestiae tenetur, hic voluptatem, voluptas odio aliquid vero totam perferendis quam animi consectetur?"              
+            },
+            {name: "Joe Smith",
+            img: "../img/avatar_3.png",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. At corrupti, ullam quo ducimus doloribus expedita labore dolorum molestiae tenetur, hic voluptatem, voluptas odio aliquid vero totam perferendis quam animi consectetur?"              
+            },
+            {name: "Jane Johnson",
+            img: "../img/avatar_4.png",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. At corrupti, ullam quo ducimus doloribus expedita labore dolorum molestiae tenetur, hic voluptatem, voluptas odio aliquid vero totam perferendis quam animi consectetur?"              
+            }
+        ]
+    }
+})
