472a3418a3d4efb72e290026be9c00668191bdc0
Hernan Rodriguez <hernan.rod.98@gmail.com>
2020-11-11 13:20:11 -0300

    Uso de Vue para cargar novedades y articulos

---
+++ js/articles.js
@@ -0,0 +1,31 @@
+articles = new Vue({
+    el:"#articles",
+    data:{
+        articles:[
+            {title: "Articulo de Investigación N1",
+            body: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti adipisci quibusdam repudiandae, accusantium aut sequi, dolore distinctio animi ad rem officia? Esse pariatur consectetur illum quod excepturi repudiandae quae tempora?
+            Tenetur corrupti debitis eligendi ducimus, eos a repellat culpa minus id enim dicta? Maiores quae voluptatem, cum dolores dignissimos voluptate. Autem esse impedit cupiditate quod veniam voluptas neque quibusdam ea!
+            Similique sit incidunt quisquam, sint itaque a libero repellendus, obcaecati labore, error atque. Quis, doloribus magnam. Beatae quasi culpa aperiam sequi quaerat. Laborum, dignissimos consequatur! Repellat veritatis accusantium iste recusandae!
+            Accusamus quod, velit tenetur accusantium reiciendis aspernatur explicabo vero quae illum at laboriosam atque minus vitae reprehenderit delectus facilis cumque tempora eligendi aliquid? Hic, eos voluptatibus tenetur vero consequatur quam.
+            Distinctio natus illum repellat nesciunt pariatur porro reprehenderit dolores placeat deleniti ullam maxime autem, sit quod nostrum voluptate! Tempora doloribus ab corrupti aliquid blanditiis vel, amet debitis quaerat eligendi laudantium!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti adipisci quibusdam repudiandae, accusantium aut sequi, dolore distinctio animi ad rem officia? Esse pariatur consectetur illum quod excepturi repudiandae quae tempora?
+            Tenetur corrupti debitis eligendi ducimus, eos a repellat culpa minus id enim dicta? Maiores quae voluptatem, cum dolores dignissimos voluptate. Autem esse impedit cupiditate quod veniam voluptas neque quibusdam ea!
+            Similique sit incidunt quisquam, sint itaque a libero repellendus, obcaecati labore, error atque. Quis, doloribus magnam. Beatae quasi culpa aperiam sequi quaerat. Laborum, dignissimos consequatur! Repellat veritatis accusantium iste recusandae!
+            Accusamus quod, velit tenetur accusantium reiciendis aspernatur explicabo vero quae illum at laboriosam atque minus vitae reprehenderit delectus facilis cumque tempora eligendi aliquid? Hic, eos voluptatibus tenetur vero consequatur quam.
+            Distinctio natus illum repellat nesciunt pariatur porro reprehenderit dolores placeat deleniti ullam maxime autem, sit quod nostrum voluptate! Tempora doloribus ab corrupti aliquid blanditiis vel, amet debitis quaerat eligendi laudantium!`              
+            },
+            {title: "Articulo de Investigación N2",
+            body: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti adipisci quibusdam repudiandae, accusantium aut sequi, dolore distinctio animi ad rem officia? Esse pariatur consectetur illum quod excepturi repudiandae quae tempora?
+            Tenetur corrupti debitis eligendi ducimus, eos a repellat culpa minus id enim dicta? Maiores quae voluptatem, cum dolores dignissimos voluptate. Autem esse impedit cupiditate quod veniam voluptas neque quibusdam ea!
+            Similique sit incidunt quisquam, sint itaque a libero repellendus, obcaecati labore, error atque. Quis, doloribus magnam. Beatae quasi culpa aperiam sequi quaerat. Laborum, dignissimos consequatur! Repellat veritatis accusantium iste recusandae!
+            Accusamus quod, velit tenetur accusantium reiciendis aspernatur explicabo vero quae illum at laboriosam atque minus vitae reprehenderit delectus facilis cumque tempora eligendi aliquid? Hic, eos voluptatibus tenetur vero consequatur quam.
+            Distinctio natus illum repellat nesciunt pariatur porro reprehenderit dolores placeat deleniti ullam maxime autem, sit quod nostrum voluptate! Tempora doloribus ab corrupti aliquid blanditiis vel, amet debitis quaerat eligendi laudantium!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti adipisci quibusdam repudiandae, accusantium aut sequi, dolore distinctio animi ad rem officia? Esse pariatur consectetur illum quod excepturi repudiandae quae tempora?
+            Tenetur corrupti debitis eligendi ducimus, eos a repellat culpa minus id enim dicta? Maiores quae voluptatem, cum dolores dignissimos voluptate. Autem esse impedit cupiditate quod veniam voluptas neque quibusdam ea!
+            Similique sit incidunt quisquam, sint itaque a libero repellendus, obcaecati labore, error atque. Quis, doloribus magnam. Beatae quasi culpa aperiam sequi quaerat. Laborum, dignissimos consequatur! Repellat veritatis accusantium iste recusandae!
+            Accusamus quod, velit tenetur accusantium reiciendis aspernatur explicabo vero quae illum at laboriosam atque minus vitae reprehenderit delectus facilis cumque tempora eligendi aliquid? Hic, eos voluptatibus tenetur vero consequatur quam.
+            Distinctio natus illum repellat nesciunt pariatur porro reprehenderit dolores placeat deleniti ullam maxime autem, sit quod nostrum voluptate! Tempora doloribus ab corrupti aliquid blanditiis vel, amet debitis quaerat eligendi laudantium!`              
+            }
+        ]
+    }
+})
---
+++ js/news.js
@@ -0,0 +1,16 @@
+novedades = new Vue({
+    el:"#novedades",
+    data:{
+        novedades:[
+            {title: "Novedad 1",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores voluptatum, molestias nihil animi recusandae beatae ullam fuga culpa harum suscipit a autem consequuntur deserunt ut dolore saepe. Eligendi, at eum!"              
+            },
+            {title: "Novedad 2",
+            body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. At corrupti, ullam quo ducimus doloribus expedita labore dolorum molestiae tenetur, hic voluptatem, voluptas odio aliquid vero totam perferendis quam animi consectetur?"              
+            },
+            {title: "Novedad 3",
+            body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. At corrupti, ullam quo ducimus doloribus expedita labore dolorum molestiae tenetur, hic voluptatem, voluptas odio aliquid vero totam perferendis quam animi consectetur?"              
+            }
+        ]
+    }
+})
